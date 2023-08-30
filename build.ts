0c5bffaeaa06b8c24d6c6e20bc61342b4c34148e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-08-30 13:50:16 +0800

    chore: update

--- build.ts
+++ build.ts
@@ -1,9 +1,9 @@
 import fsp from 'fs/promises'
 import { jsShell } from 'lazy-js-utils'
-(async() => {
+(async () => {
   // build:js
   console.log('js building...')
-  await jsShell('unbuild')
+  await jsShell('tsup ./src/index.ts --format cjs,esm --dts --clean --minify')
   console.log('js build success')
   console.log('css building...')
   const css = await fsp.readFile('./src/index.css', 'utf-8')
