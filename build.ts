931f671aaa13310ef1bb7e1031713f9a6610ee7d
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-01-22 10:02:39 +0800

    chore: update

--- build.ts
+++ build.ts
@@ -1,19 +1,19 @@
 import fsp from 'fs/promises'
-import { jsShell } from 'lazy-js-utils'
-;(async () => {
-  // build:js
-  // eslint-disable-next-line no-console
-  console.log('js building...')
-  await jsShell('tsup ./src/index.ts --format cjs,esm --dts --clean --minify')
-  // eslint-disable-next-line no-console
-  console.log('js build success')
-  // eslint-disable-next-line no-console
-  console.log('css building...')
-  const css = await fsp.readFile('./src/index.css', 'utf-8')
-  await fsp.writeFile(
-    './dist/index.css',
-    css.replace(/\n/g, '').replace(/\s+/g, ' '),
-  )
-  // eslint-disable-next-line no-console
-  console.log('css build success')
-})()
+import { jsShell } from 'lazy-js-utils/dist/node'
+  ; (async () => {
+    // build:js
+    // eslint-disable-next-line no-console
+    console.log('js building...')
+    await jsShell('tsup ./src/index.ts --format cjs,esm --dts --clean --minify')
+    // eslint-disable-next-line no-console
+    console.log('js build success')
+    // eslint-disable-next-line no-console
+    console.log('css building...')
+    const css = await fsp.readFile('./src/index.css', 'utf-8')
+    await fsp.writeFile(
+      './dist/index.css',
+      css.replace(/\n/g, '').replace(/\s+/g, ' '),
+    )
+    // eslint-disable-next-line no-console
+    console.log('css build success')
+  })()
